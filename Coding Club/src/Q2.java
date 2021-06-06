3404f42fa81b39b631eaa3a5b25a9ad736a425df
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2021-06-06 19:17:04 +0530

    Update Q2.java

--- Coding Club/src/Q2.java
+++ Coding Club/src/Q2.java
@@ -5,46 +5,119 @@ public class Q2 {
 		// TODO Auto-generated method stub
 	 int count = 0;
 	  Scanner in = new Scanner(System.in);
-	  System.out.println("Enter number of elements you want to enter: ");
+	  System.out.println("Enter number of rows you want to enter: ");
+	  int m = in.nextInt();
+	  System.out.println("Enter number of columns you want to enter: ");
 	  int n = in.nextInt();
-    int[] num = new int [n];
-    for(int i =0; i<num.length ; i++) {
-  	  Scanner sc = new Scanner(System.in);
-  	  System.out.println("Enter element "+(i+1)+": ");
-  	  num[i]=sc.nextInt();
+     int[][] num = new int [m][n];
+    for(int i =0; i<m ; i++) {
+    	for(int j =0;j<n;j++) {
+    	  	  Scanner sc = new Scanner(System.in);
+    	  	  System.out.println("Enter element "+"["+i+","+j+"]"+": ");
+    	  	  num[i][j]=sc.nextInt();
+    	}
+    }
+	System.out.println("Original matrix:");
+    for(int i =0; i<m ; i++) {
+    	for(int j =0;j<n;j++) {
+    		System.out.print(num[i][j]+" ");
+    	}
+    	System.out.println("");
     }
 	 Scanner sc = new Scanner(System.in); 
-	 System.out.println("Your choice: ");
+	 System.out.println("Your choice (ascending/descending): ");
      String s= sc.next();
      switch(s) {
-     case "ascending" : 	 
-     for(int i =0;i<num.length-1;i++) {
-         for(int j=0;j<num.length-i-1;j++) {
-        	 if(num[j]>num[j+1]) {
-        		 int temp=0;
-        		 temp=num[j];
-        		 num[j]=num[j+1];
-        		 num[j+1]=temp;
-        	 }
+     case "ascending" : 
+    	 System.out.println("Row sorting / Column sorting");
+    	 String z = sc.next();
+    	 switch(z) {
+    	 case "row": 
+    	      for (int i = 0; i < m; i++) {
+    	          for(int j=0;j<n;j++) {
+    	              for (int k = 0; k < num[i].length - j - 1; k++) {
+    	                  if (num[i][k] > num[i][k + 1]) {
+    	                      int t = num[i][k];
+    	                      num[i][k] = num[i][k + 1];
+    	                      num[i][k + 1] = t;
+    	                  }
+    	              }
+    	          }
+    	      }
+    	         for(int k =0;k<m;k++) {
+    	         	for(int l =0;l<n;l++) {
+    	                 System.out.print(num[k][l]+" ");
+    	         	} 
+    	         	System.out.println("");
+    	         }
+    	       break;
+    	 case "column" :
+ 	        for(int i = 0; i < m; i++){
+ 	            for(int j = 0; j < n; j++){
+ 	                for(int k = 0; k < m - 1 - j; k++){ 
+ 	                    if(num[k][i] > num[k + 1][i]){
+ 	                        int temp = num[k][i];
+ 	                        num[k][i] = num[k + 1][i];
+ 	                        num[k + 1][i] = temp;
+ 	                    }
+ 	                }
+ 	            }
+ 	        }
+ 	        System.out.println("Sorted matrix:");
+ 	        for(int i = 0; i < m; i++){
+ 	            for(int j = 0; j < n; j++){
+ 	                System.out.print(num[i][j] + " ");
+ 	            }
+ 	            System.out.println();
+ 	        }
+    	 }
+    	 break;
+
+     case "descending":
+    	 System.out.println("Row sorting / Column sorting");
+    	 String w = sc.next();
+    	 switch(w) {
+    	 case "row":	 
+         for (int i = 0; i < m; i++) {
+             for(int j=0;j<n;j++) {
+                 for (int k = 0; k < num[i].length - j - 1; k++) {
+                     if (num[i][k] < num[i][k + 1]) {
+                         int t = num[i][k];
+                         num[i][k] = num[i][k + 1];
+                         num[i][k + 1] = t;
+                     }
+                 }
+             }
          }
-     }
-        for(int k =0;k<num.length;k++) {
-            System.out.println(num[k]); 
-        }
-      break;
-     case "descending":      for(int i =0;i<num.length-1;i++) {
-         for(int j=0;j<num.length-i-1;j++) {
-        	 if(num[j]<num[j+1]) {
-        		 int temp=0;
-        		 temp=num[j];
-        		 num[j]=num[j+1];
-        		 num[j+1]=temp;
-        	 }
-         }
-     }
-        for(int k =0;k<num.length;k++) {
-            System.out.println(num[k]); 
-        }
+         System.out.println("Sorted matrix:");
+            for(int k =0;k<m;k++) {
+            	for(int l =0;l<n;l++) {
+                    System.out.print(num[k][l]+" ");
+            	} 
+            	System.out.println("");
+            }
+            break;
+    	 case "column" :
+    	        for(int i = 0; i < m; i++){
+    	            for(int j = 0; j < n; j++){
+    	                for(int k = 0; k < m - 1 - j; k++){ 
+    	                    if(num[k][i] > num[k + 1][i]){
+    	                        int temp = num[k][i];
+    	                        num[k][i] = num[k + 1][i];
+    	                        num[k + 1][i] = temp;
+    	                    }
+    	                }
+    	            }
+    	        }
+    	        System.out.println("Sorted matrix:");
+    	        for(int i = 0; i < m; i++){
+    	            for(int j = 0; j < n; j++){
+    	                System.out.print(num[i][j] + " ");
+    	            }
+    	            System.out.println();
+    	        }
+    	        break;
+    	 }
      }
 	}
 }
