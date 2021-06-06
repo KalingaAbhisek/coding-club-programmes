e68204e4190a781d5948a7ac2e85022ac82d87da
KALINGA ABHISEK <kalingaabhisek@outlook.com>
2021-06-06 18:35:41 +0530

    Update Q10.java

--- Coding Club/src/Q10.java
+++ Coding Club/src/Q10.java
@@ -3,9 +3,11 @@ public class Q10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	  int z;
       Scanner in = new Scanner(System.in);
       System.out.println("Enter the number: ");
-      String n = in.next();
+      z = in.nextInt();
+      String n=Integer.toString(z);
       String m = n+n;
       String o = n+n+n;
       System.out.println(Integer.parseInt(n)+Integer.parseInt(m)+Integer.parseInt(o));
